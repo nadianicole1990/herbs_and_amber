7a6a0a3731a39d82d0693d6f1086205cfbced465
nadianicole1990 <157327606+nadianicole1990@users.noreply.github.com>
2024-09-04 22:55:36 -0400

    Create, Edit, and Delete all reflect basic functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('tinctures/', views.tinctures_index, name='index'),
     path('tinctures/<int:tincture_id>/', views.tinctures_detail, name='detail'),
+    path('tinctures/create/', views.TinctureCreate.as_view(), name='tincture_create'),
+    path('tinctures/<int:pk>/update/', views.TinctureUpdate.as_view(), name='tinctures_update'),
+    path('tinctures/<int:pk>/delete/', views.TinctureDelete.as_view(), name='tinctures_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Tincture
 
 # Create your views here.
@@ -15,3 +16,17 @@ def tinctures_index(request):
 def tinctures_detail(request, tincture_id):
     tincture = Tincture.objects.get(id=tincture_id)
     return render(request, 'tinctures/detail.html', { 'tincture': tincture })
+
+class TinctureCreate(CreateView):
+    model = Tincture
+    fields = '__all__'
+    success_url = '/tinctures/'
+    
+class TinctureUpdate(UpdateView):
+    model = Tincture
+    fields = ['herbs', 'solvent']
+    success_url = '/tinctures/'
+    
+class TinctureDelete(DeleteView):
+    model = Tincture
+    success_url = '/tinctures/'
